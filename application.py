3e1189492edb05e53b15d69b9fc668b1efd710ce
Moises Guerra <moiimkii@gmail.com>
2014-12-03 13:07:27 -0600

    Factorial completo

--- application.py
+++ application.py
@@ -1,3 +1,13 @@
-# Aqui escribe tu codigo
-
-print "Hello World"
+#******************************************************************************
+#*************************** FACTORIAL ****************************************
+print "*** FACTORIAL ***"
+print "Calculo del factorial de el numero que desee."
+numero =input("Ingrese el numero que desee: ")
+def factorial(numero):
+    if numero == 1:
+        return 1
+    else:
+        respuesta = numero * factorial(numero-1)
+        return respuesta
+print"El calculo del factorial de %s es: "%(numero)
+print(factorial(numero))
